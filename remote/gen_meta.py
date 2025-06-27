d173e481ec6d2899d431fbbd7c8177f4817e7325
jihan-xia <zxia15@jh.edu>
2025-06-27 09:23:52 -0400

    modified remote end data processing

--- remote/gen_meta.py
+++ remote/gen_meta.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     parser.add_argument("--directory", type=str, default="/data/topology/ark/data/team-probing/list-7.allpref24/team-1/daily/2024")
     parser.add_argument("--meta_dir", type=str, default="data/iso-3166-countries-with-regional-codes.csv")
     parser.add_argument('--mode', type=str, default='vantage-point')
+    parser.add_argument('--operation', type=str, default='union')
     args = parser.parse_args()
     
     if not os.path.exists(args.directory):
@@ -63,15 +64,23 @@ if __name__ == '__main__':
         if len(persistent_cn) == 0:
             persistent_cn = set(sorted_vp_map.keys())
         else:
-            persistent_cn.intersection(sorted_vp_map.keys())
+            if args.operation == 'intersect':
+                persistent_cn.intersection(sorted_vp_map.keys())
+            elif args.operation == 'union':
+                persistent_cn.union(sorted_vp_map.keys())
         
         all_vps = []
         for cn, vps in sorted_vp_map.items():
+            if cn not in {'South Africa(za)', 'Tanzania, United Republic of(tz)', 'Mauritius(mu)', 'Kenya(ke)', 'Ghana(gh)', 'Israel(il)', 'Gambia(gm)', 'Madagascar(mg)'}:
+                continue
             all_vps.extend([f'{cn}-{vp}' for vp in vps])
         if len(persistent_vp) == 0:
             persistent_vp = set(all_vps)
         else:
-            persistent_vp.intersection(all_vps)
+            if args.operation == 'intersect':
+                persistent_vp.intersection(all_vps)
+            elif args.operation == 'union':
+                persistent_vp.union(all_vps)
     if args.mode == 'country':
         print(persistent_cn)
     elif args.mode == 'vantage-point':
--- remote/read_data.py
+++ remote/read_data.py
@@ -1,4 +1,4 @@
-import os, argparse, logging, gzip, json
+import os, argparse, logging, gzip, json, re
 from parser import WartsDumpParser
 from utils import sort_measure_cycle
 
@@ -10,13 +10,23 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--directory", type=str, default="/data/topology/ark/data/team-probing/list-7.allpref24/team-1/daily/2024")
-    parser.add_argument("--spec", type=str, required=True)
+    parser.add_argument('--country_spec', type=str, required=True)
+    parser.add_argument('--airport_spec', type=str, default=None)
+    parser.add_argument('--probe_num_spec', type=int, default=None)
     args = parser.parse_args()
+
+    if args.airport_spec and args.probe_num_spec:
+        label = rf'{args.airport_spec}{args.probe_num_spec}-{args.country_spec}\.team-probing' if args.probe_num_spec != -1 else rf'{args.airport_spec}-{args.country_spec}\.team-probing'
+    elif args.airport_spec:
+        label = rf'{args.airport_spec}\d*-{args.country_spec}\.team-probing'
+    else:
+        label = rf'{args.country_spec}\.team-probing'
 
     if not os.path.exists(args.directory):
         logger.debug(f"address {args.directory} does not exit")
         exit(0)
-    
+
+    label = re.compile(label) 
     # get a specific path
     cycles = os.listdir(args.directory)
     sort_measure_cycle(cycles)
@@ -24,14 +34,22 @@ if __name__ == '__main__':
 
     for cycle in cycles:
         path = os.path.join(args.directory, cycle)
-        data = [inst for inst in os.listdir(path) if inst.startswith(args.spec)]
-        
+        if not os.path.isdir(path):
+            continue
+        data = [inst for inst in os.listdir(path) if label.search(inst)]
+        print(f'found {len(data)} probing instance in cycle {cycle}: {data}') 
         for inst in data:
             p = WartsDumpParser(path, inst)
             print(f"querying capture {inst}")
             graph_data[inst] = p.get_data('trace')
-    
-    with gzip.open(f'data/all-meta-from-{args.spec}.jsonl.gz', 'wt', encoding='utf-8') as f:
+   
+    output_dir = 'data/all-meta-from-' + args.country_spec
+    if args.airport_spec:
+        output_dir += '-' + args.airport_spec
+    if args.probe_num_spec:
+        output_dir += '-' + str(args.probe_num_spec)
+    output_dir += 'jsonl.gz'
+    with gzip.open(output_dir, 'wt', encoding='utf-8') as f:
         for key, value in graph_data.items():
             json_line = json.dumps({key: value})
             f.write(json_line + '\n')
